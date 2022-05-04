6c5fab8bc911f167ca6e6d82a9091d9e13c64172
Max Lettenberger <maxlettenberger@gmail.com>
2022-05-04 10:40:32 -0400

    remove column dividers

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import logo from './logo.svg';
-import './App.css';
-import { DataGrid } from '@mui/x-data-grid';
+import logo from "./logo.svg";
+import "./App.css";
+import { DataGrid } from "@mui/x-data-grid";
 
 function App() {
   return (
@@ -11,58 +11,157 @@ function App() {
 }
 
 const columns = [
-  { field: 'fullName', headerName: 'Full Name', width: 150 },
   {
-    field: 'jobRole',
-    headerName: 'Job Role',
+    field: "fullName",
+    headerName: "Full Name",
     width: 150,
-    editable: true,
-  },
-  {
-    field: 'status',
-    headerName: 'Status',
-    width: 150,
-    editable: true,
-  },
-  {
-    field: 'progress',
-    headerName: 'Progress',
-    type: 'number',
-    width: 110,
-    editable: true,
-  },
-  {
-    field: 'dueDate',
-    headerName: 'Due Date',
-    description: 'This column has a value getter and is not sortable.',
     sortable: false,
-    width: 160
+    disableColumnMenu: true,
+    headerClassName: "lastcolumnSeparator",
+  },
+  {
+    field: "jobRole",
+    headerName: "Job Role",
+    width: 150,
+    sortable: false,
+    disableColumnMenu: true,
+    headerClassName: "lastcolumnSeparator",
+  },
+  {
+    field: "status",
+    headerName: "Status",
+    width: 150,
+    sortable: false,
+    disableColumnMenu: true,
+    headerClassName: "lastcolumnSeparator",
+  },
+  {
+    field: "progress",
+    headerName: "Progress",
+    type: "number",
+    width: 110,
+    sortable: false,
+    disableColumnMenu: true,
+    headerClassName: "lastcolumnSeparator",
+    // editable: true,
+  },
+  {
+    field: "dueDate",
+    headerName: "Due Date",
+    width: 160,
+    sortable: false,
+    disableColumnMenu: true,
+    headerClassName: 'lastcolumnSeparator',
   },
 ];
 
 const rows = [
-  { id: 1, fullName: "Hoaran Li", jobRole: 'Java Developer', status: 'New', progress: 35, dueDate: "a" },
-  { id: 2, fullName: "Peter Sun", jobRole: 'React Developer', status: 'Pending', progress: 35, dueDate: "a" },
-  { id: 3, fullName: "Jack Rabbit", jobRole: 'Angular Developer', status: 'Pending', progress: 35, dueDate: "a" },
-  { id: 4, fullName: "Tumi Sum", jobRole: 'Java Developer', status: 'Pending', progress: 35, dueDate: "a" },
-  { id: 5, fullName: "Eelee Brunson", jobRole: 'React Developer', status: 'Pending', progress: 35, dueDate: "a" },
-  { id: 6, fullName: "Sum Dillon", jobRole: 'Angular Developer', status: 'Pending', progress: 35, dueDate: "a" },
-  { id: 7, fullName: "Vince Crier", jobRole: 'Java Developer', status: 'Pending', progress: 35, dueDate: "a" },
-  { id: 8, fullName: "Tum Basco", jobRole: 'React Developer', status: 'Pending', progress: 35, dueDate: "a" },
-  { id: 9, fullName: "Ser Terir", jobRole: 'Angular Developer', status: 'Pending', progress: 35, dueDate: "a" },
-  { id: 10, fullName: "Tus Sommer", jobRole: 'Java Developer', status: 'Pending', progress: 35, dueDate: "a" },
-  { id: 11, fullName: "Emily Bunchin", jobRole: 'React Developer', status: 'Pending', progress: 35, dueDate: "a" },
-  { id: 12, fullName: "Ting Duzzo", jobRole: 'Angular Developer', status: 'Pending', progress: 35, dueDate: "a" },
+  {
+    id: 1,
+    fullName: "Hoaran Li",
+    jobRole: "Java Developer",
+    status: "New",
+    progress: 0,
+    dueDate: "23 October 2022",
+  },
+  {
+    id: 2,
+    fullName: "Peter Sun",
+    jobRole: "React Developer",
+    status: "Pending",
+    progress: 90,
+    dueDate: "18 November 2022",
+  },
+  {
+    id: 3,
+    fullName: "Jack Rabbit",
+    jobRole: "Angular Developer",
+    status: "Pending",
+    progress: 20,
+    dueDate: "23 October 2022",
+  },
+  {
+    id: 4,
+    fullName: "Tumi Sum",
+    jobRole: "Java Developer",
+    status: "Pending",
+    progress: 80,
+    dueDate: "18 November 2022",
+  },
+  {
+    id: 5,
+    fullName: "Eelee Brunson",
+    jobRole: "React Developer",
+    status: "Pending",
+    progress: 0,
+    dueDate: "23 October 2022",
+  },
+  {
+    id: 6,
+    fullName: "Sum Dillon",
+    jobRole: "Angular Developer",
+    status: "Pending",
+    progress: 90,
+    dueDate: "18 November 2022",
+  },
+  {
+    id: 7,
+    fullName: "Vince Crier",
+    jobRole: "Java Developer",
+    status: "Pending",
+    progress: 20,
+    dueDate: "23 October 2022",
+  },
+  {
+    id: 8,
+    fullName: "Tum Basco",
+    jobRole: "React Developer",
+    status: "Pending",
+    progress: 80,
+    dueDate: "18 November 2022",
+  },
+  {
+    id: 9,
+    fullName: "Ser Terir",
+    jobRole: "Angular Developer",
+    status: "Pending",
+    progress: 0,
+    dueDate: "23 October 2022",
+  },
+  {
+    id: 10,
+    fullName: "Tus Sommer",
+    jobRole: "Java Developer",
+    status: "Pending",
+    progress: 90,
+    dueDate: "18 November 2022",
+  },
+  {
+    id: 11,
+    fullName: "Emily Bunchin",
+    jobRole: "React Developer",
+    status: "Pending",
+    progress: 20,
+    dueDate: "23 October 2022",
+  },
+  {
+    id: 12,
+    fullName: "Ting Duzzo",
+    jobRole: "Angular Developer",
+    status: "Pending",
+    progress: 80,
+    dueDate: "18 November 2022",
+  },
 ];
 
 function DataGridDemo() {
   return (
-    <div style={{ height: 400, width: '100%' }}>
+    <div style={{ height: 600, width: "100%" }}>
       <DataGrid
         rows={rows}
         columns={columns}
-        pageSize={5}
-        rowsPerPageOptions={[5]}
+        pageSize={9}
+        rowsPerPageOptions={[9]}
         checkboxSelection
         disableSelectionOnClick
       />
