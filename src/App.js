99e641cd8c527bf12c722f2946ccb5e9e82e75ce
Max Lettenberger <maxlettenberger@gmail.com>
2022-05-05 13:36:14 -0400

    add layout

--- src/App.js
+++ src/App.js
@@ -1,11 +1,32 @@
 import logo from "./logo.svg";
 import "./App.css";
-import AuraDataGrid from './components/AuraDataGrid/AuraDataGrid'
+import AuraDataGrid from "./components/AuraDataGrid/AuraDataGrid";
 
 function App() {
   return (
     <div className="App">
-      <AuraDataGrid />
+      <div className="layout">
+        <div className="layout-left">
+          <div className="layout-left__charts">
+            <div className="chart"></div>
+            <div className="chart"></div>
+            <div className="chart"></div>
+          </div>
+          <div className="layout-left__table">
+            <AuraDataGrid />
+          </div>
+        </div>
+        <div className="layout-right">
+          <div className="layout-right__calendars">
+            <div className="calendar"></div>
+            <div className="calendar"></div>
+            <div className="calendar"></div>
+            <div className="calendar"></div>
+            <div className="calendar"></div>
+            <div className="calendar__edit"></div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
